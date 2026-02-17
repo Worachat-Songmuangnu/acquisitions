eb0b6f0d393b6b7423b78da9797afa5443fe9aa4
Worachat <worachat.smn@gmail.com>
2026-02-17 22:43:00 +0700

    Structure: spilt indexjs

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.status(200).send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+export default app;
