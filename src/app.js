3c6978dbda72eed2d3437e368a88fb719c7bf9d0
Worachat <worachat.smn@gmail.com>
2026-02-17 22:47:19 +0700

    Fix: typo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 
-app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).send("Hello From Acquisitions!");
 });
 
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import dotenv from "dotenv";
-import "./server"
+import "./server.js"
