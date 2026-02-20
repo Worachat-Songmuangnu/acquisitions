e054c9e7c7ee1778c78bd565ca8c51760c2b217f
Worachat <worachat.smn@gmail.com>
2026-02-21 00:04:07 +0700

    Add: zod validation user auth

---
+++ src/validations/auth.validation.js
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+export const signUpSchema = z.object({
+    name: z.string().trim().min(2).max(255),
+    email: z.email().toLowerCase().trim(),
+    password: z.string().min(8).max(255).trim(),
+    role: z.enum(['admin', 'user']).default('user'),});
+
+export const signInSchema = z.object({
+    email: z.email().toLowerCase().trim(),
+    password: z.string().min(1),
+});
+
